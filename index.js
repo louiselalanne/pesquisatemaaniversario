8abe1b0b962be96d2ce52b551317f908132698ae
Louise Lalanne <100588945+louiselalanne@users.noreply.github.com>
2022-03-26 09:17:03 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+onEvent("botaoPesquisa", "click", function( ) {
+  setScreen("telaPesquisa1");
+});
+var DadosTemaAnv = {};
+onEvent("botaoProximo", "click", function( ) {
+  DadosTemaAnv.nome = getText("textoInsercaoNome");
+  DadosTemaAnv.idade = getNumber("textoInsercaoIdade");
+  setScreen("telaPesquisa2");
+});
+onEvent("botaoDD", "click", function( ) {
+  DadosTemaAnv.tema = "Debaixo D'água";
+  setProperty("botaoDD", "background-color", "green");
+  setProperty("botaoP", "background-color", "#fef6e7");
+  setProperty("botaoS", "background-color", "#fef6e7");
+  setImageURL("imagemT", "mermaid.png");
+});
+onEvent("botaoP", "click", function( ) {
+  DadosTemaAnv.tema = "Piratas";
+  setProperty("botaoP", "background-color", "green");
+  setProperty("botaoDD", "background-color", "#fef6e7");
+  setProperty("botaoS", "background-color", "#fef6e7");
+  setImageURL("imagemT", "pirate.png");
+});
+onEvent("botaoS", "click", function( ) {
+  DadosTemaAnv.tema = "Super-herói";
+  setProperty("botaoS", "background-color", "green");
+  setProperty("botaoDD", "background-color", "#fef6e7");
+  setProperty("botaoP", "background-color", "#fef6e7");
+  setImageURL("imagemT", "superheroe.png");
+});
+onEvent("botaoEnviar", "click", function( ) {
+  setScreen("telaResultado");
+  console.log(DadosTemaAnv);
+  createRecord("Pesquisa", DadosTemaAnv);
+  setText("etiquetaN", "Nome:"+DadosTemaAnv.nome);
+  setText("etiquetaI", "Idade:"+DadosTemaAnv.idade);
+});
